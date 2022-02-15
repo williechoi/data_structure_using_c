a064e09d475c375f496681987eda76c081dd77eb
Choih <choihyungsuk@gmail.com>
2022-02-15 09:57:22 -0500

    Doubly Linked List - Practice using two dummy nodes (head, tail)

---
+++ Ch05/DBLinkedList2/DBLinkedList.h
@@ -0,0 +1,36 @@
+#ifndef __DB_LINKED_LIST_H__
+#define __DB_LINKED_LIST_H__
+
+#define TRUE        1
+#define FALSE       0
+
+typedef int Data;
+
+typedef struct _node 
+{
+    Data data;              // typedef int Data;
+    struct _node* next;
+    struct _node* prev;
+} Node;
+
+typedef struct _DLinkedList
+{
+    Node* head;
+    Node* tail;
+    Node* cur;
+    int numOfData;
+} DBLinkedList;
+
+typedef DBLinkedList List;
+
+void ListInit(List* plist);
+void LInsert(List* plist, Data data);
+
+int LFirst(List* plist, Data *pdata);
+int LNext(List* plist, Data* pdata);
+
+Data LRemove(List* plist);
+
+int LCount(List* plist);
+
+#endif
